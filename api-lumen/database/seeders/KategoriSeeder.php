795c064ecb93d6f751ff1c4bc8d77d1e23c5a0e8
SayyidDeo134 <sayyiddeo.real@gmail.com>
2021-03-06 14:06:59 +0700

    Video 7 Lumen

--- api-lumen/database/seeders/KategoriSeeder.php
+++ api-lumen/database/seeders/KategoriSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Kategori;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class KategoriSeeder extends Seeder
 {
@@ -14,5 +16,13 @@ class KategoriSeeder extends Seeder
     public function run()
     {
         //
+        $faker = Faker::create();
+        for ($i = 0; $i < 5; $i++) {
+            $data = [
+                'kategori'      =>  $faker->word()
+            ];
+
+            Kategori::create($data);
+        }
     }
 }
