3674bbe5132f4c4cd9e7bb463fbcc25690efc30f
SayyidDeo134 <sayyiddeo.real@gmail.com>
2021-03-07 19:33:52 +0700

    Video 12 React JS

--- api-lumen/bootstrap/app.php
+++ api-lumen/bootstrap/app.php
@@ -74,9 +74,9 @@ $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
